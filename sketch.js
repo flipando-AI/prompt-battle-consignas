7f202bdac32b3985e8fd8bfcf83b79741055c7c1
git config credential.helper <yvesfogel@gmail.com>
2023-01-09 14:33:38 -0500

    btns abajo

--- sketch.js
+++ sketch.js
@@ -1,33 +1,44 @@
 let idxActual = 0;
 let imgRightBtn;
-let tiempoInicial = 59;
+let playBtn;
+let resetBtn;
+let pauseBtn;
+let tiempoInicial = 3;
 let counter = tiempoInicial;
 let nuestroFont;
 
 let rightbtnX;
 let leftbtnX;
+let rightbtnY;
+let leftbtnY;
+
+let playing = false;
 
 function preload(){
   imgRightBtn = loadImage("right.png");
   nuestroFont = loadFont("Manrope-VariableFont_wght.ttf");
+  playBtn = loadImage("play.png");
+  pauseBtn = loadImage("pause.png");
+  resetBtn = loadImage("reset.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(60);
+  frameRate(30);  
+
+
 }
 
 function draw() {
   textFont(nuestroFont);
   background(0);
   fill(255);
-  textSize(64);
   textWrap(WORD);
   rectMode(CENTER);
   textAlign(CENTER);
   let textActual = listaTextos[idxActual%listaTextos.length];
-  let textoY = height/2;
-  text(textActual, width/2, textoY, width*0.75);
+  let textoY = height/2.6;
+  text(textActual, width/2, textoY, width);
   
   imageMode(CENTER);
   
@@ -37,30 +48,52 @@ function draw() {
     secs = "0" + secs;
   }
   let txtTimer = mins + ":" + secs;
-  text(txtTimer, width/2, height * 0.1);
   
+  textSize(160);
+  textStyle(BOLD);
+
+
   if(counter <= 0){
-    background(0, 0, 0);
-    text("TIME OUT!", width/2, height/2 + 20);
+    text("TIME OUT!", width/2, height * 0.2);
+    playing = false;
+  } else{
+    text(txtTimer, width/2, height * 0.2);
   }
+
+  textSize(120);
+  textStyle(NORMAL);
+
+
   
-  
-  let btnsPct = map(windowWidth, 0, 2000, 0.15, 0.02);
-  rightbtnX = width*(1-btnsPct)
-  leftbtnX = width*(btnsPct)
+  rightbtnX = width*0.8
+  leftbtnX = width*0.2
+  rightbtnY = height * 0.9
+  leftbtnY = height * 0.9
 
 
   tint(255, 255, 255, rightOpacity());
-  image(imgRightBtn, rightbtnX, height/2, 100, 100);
+  image(imgRightBtn, rightbtnX, height*0.9, 100, 100);
+
+  tint(255, 255, 255, playOpacity());
+  if(playing){
+    image(pauseBtn, width*0.4, height*0.9, 100, 100)
+  } else{
+    image(playBtn, width*0.4, height*0.9, 100, 100)
+  }
+
+  tint(255, 255, 255, resetOpacity());
+  image(resetBtn, width*0.6, height*0.9, 100, 100)
+
+
   
   push();
   tint(255, 255, 255, leftOpacity());
-  translate(leftbtnX, height/2);
+  translate(leftbtnX, height*0.9);
   scale(-1, 1);
   image(imgRightBtn, 0, 0, 100, 100);
   pop();
   
-  if(frameCount % 60 == 0){
+  if(frameCount % 30 == 0 && playing){
     counter--;
   }
 }
@@ -82,33 +115,70 @@ function rightOpacity(){
   }
 }
 
+function playOpacity(){
+  if(mouseIsInPlayBtn()){
+    return 255;
+  } else{
+    return 80;
+  }
+}
+
+function resetOpacity(){
+  if(mouseIsInResetBtn()){
+    return 255;
+  } else{
+    return 80;
+  }
+}
+
 function mouseIsInRightBtn(){
-  return (dist(mouseX, mouseY, rightbtnX, height/2) < 50);
+  return (dist(mouseX, mouseY, rightbtnX, rightbtnY) < 50);
 }
 
 function mouseIsInLeftBtn(){
-  return (dist(mouseX, mouseY, leftbtnX, height/2) < 50);
+  return (dist(mouseX, mouseY, leftbtnX, leftbtnY) < 50);
+}
+
+function mouseIsInPlayBtn(){
+  if(mouseX > width*0.4 - 50 && mouseX < width*0.4 + 50 && mouseY > height*0.9 - 50 && mouseY < height*0.9 + 50){
+    return true;
+  } else{
+    return false;
+  }
+}
+
+function mouseIsInResetBtn(){
+  return (dist(mouseX, mouseY, width*0.6, height*0.9) < 50);
 }
 
 function mousePressed(){
   if(mouseIsInRightBtn()){
     nextIdx();
-  }
-  
-  if(mouseIsInLeftBtn()){
+  } else if(mouseIsInLeftBtn()){
     prevIdx();
+  } else if(mouseIsInPlayBtn()){
+    playing = !playing;
+    if(counter <= 0){
+      counter = tiempoInicial;
+    }
+    
+  } else if(mouseIsInResetBtn){
+    console.log("aaaaaa");
+    playing = false;
+    counter = tiempoInicial;
   }
 }
 
 function nextIdx(){
-  counter = tiempoInicial;
+  if(!playing){
+    counter = tiempoInicial;
+  }
 
   idxActual ++;
 }
 
 
 function prevIdx(){
-  counter = tiempoInicial;
 
   idxActual--;
   if(idxActual < 0){
@@ -119,12 +189,4 @@ function prevIdx(){
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-}
-
-function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-    idxActual --;
-  } else if (keyCode === RIGHT_ARROW) {
-    nextIdx();
-  }
 }
--- textos.js
+++ textos.js
@@ -1,7 +1,7 @@
-let listaTextos = ["Place yourself in a picture with your spirit animal.",
-                   "Create a hybrid of your favorite place and your favorite dish.",
-                  "Create your ideal working-holiday scenario.",
-                  "Steal the style of your favorite artist to create a portrait of your opponent.",
-                  "Imagine a networking event like Punta Tech Meetup for children.",
-                  "Build Punta del Este ideal landscape in 2050 with your favorite architect style.",
-                  "Imagine what is happening inside Dall-E software while it is generating your image."]
+let listaTextos = ["#1. Place yourself in a picture with your spirit animal.",
+                   "#2. Create a hybrid of your favorite place and your favorite dish.",
+                  "#3. Create your ideal working-holiday scenario.",
+                  "#4. Steal the style of your favorite artist to create a portrait of your opponent.",
+                  "#5. Imagine a networking event like Punta Tech Meetup for children.",
+                  "#6. Build Punta del Este ideal landscape in 2050 with your favorite architect style.",
+                  "#7. Imagine what is happening inside Dall-E software while it is generating your image."]
